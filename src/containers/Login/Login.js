20279347895fb46ffb19a5a707e0fa4fef888839
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-25 11:05:45 -0400

    Reformatted file

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,47 +1,47 @@
-import React from 'react';
+import React from "react";
 
 class Login extends React.Component {
-    state = {
-        inputs: {
-          email: "",
-          password: ""
-        }
-      };
-
-    handleInputChange = e => {
-        const { inputs } = {...this.state};
-        inputs[e.target.name] = e.target.value.trim();
-
-        this.setState({ inputs });
+  state = {
+    inputs: {
+      email: "",
+      password: ""
     }
+  };
 
-    render () {
-        const { inputs } = this.state;
-        const inputsArray = Object.entries(inputs);
+  handleInputChange = e => {
+    const { inputs } = { ...this.state };
+    inputs[e.target.name] = e.target.value.trim();
 
-        return (
-            <form>
-                {inputsArray.map(([inputName, inputValue], index) => {
-                    const inputType =
-                        inputName.toLowerCase() === "password" ? "password" : "text";
+    this.setState({ inputs });
+  };
 
-                    return (
-                        <input
-                        key={index}
-                        onChange={this.handleInputChange}
-                        type={inputType}
-                        value={inputValue}
-                        name={inputName}
-                        placeholder={inputName}
-                        required
-                        min='1'
-                        />
-                    );
-                })}
-                <button type='submit'>Login</button>
-            </form>
-        );
-    }
+  render() {
+    const { inputs } = this.state;
+    const inputsArray = Object.entries(inputs);
+
+    return (
+      <form>
+        {inputsArray.map(([inputName, inputValue], index) => {
+          const inputType =
+            inputName.toLowerCase() === "password" ? "password" : "text";
+
+          return (
+            <input
+              key={index}
+              onChange={this.handleInputChange}
+              type={inputType}
+              value={inputValue}
+              name={inputName}
+              placeholder={inputName}
+              required
+              min="1"
+            />
+          );
+        })}
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
 }
 
 export default Login;
