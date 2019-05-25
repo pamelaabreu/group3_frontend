f70e88f9903f99c2784a3bc03fc840bc0004aa0b
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-25 10:59:55 -0400

    Created JSX for the form and submit button

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -10,10 +10,27 @@ class Login extends React.Component {
 
     render () {
         const { inputs } = this.state;
-        
+        const inputsArray = Object.entries(inputs);
+
         return (
             <form>
-                
+                {inputsArray.map(([inputName, inputValue], index) => {
+                    const inputType =
+                        inputName.toLowerCase() === "password" ? "password" : "text";
+
+                    return (
+                        <input
+                        key={index}
+                        type={inputType}
+                        value={inputValue}
+                        name={inputName}
+                        placeholder={inputName}
+                        required
+                        min='1'
+                        />
+                    );
+                })}
+                <button type='submit'>Login</button>
             </form>
         );
     }
