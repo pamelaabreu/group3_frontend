0154c746f02e73f2c048e96a666dccde92d6eec7
Robert Abreu <robertabreu@pursuit.org>
2019-05-31 23:31:52 -0400

    bagselector now passes up the name of the bags to update state

--- src/components/BagSelectorCard/BagSelectorCard.js
+++ src/components/BagSelectorCard/BagSelectorCard.js
@@ -4,7 +4,7 @@ export default (props => {
     const { bag_type, handleOnClick, countAndKey: {count, key}} = props;
     return (
           <div className="col-3 mx-1 mt-2 card p-0 pack--bag-width">
-            <button className="card-body row" onClick={handleOnClick("bag", key)}>
+            <button className="card-body row" onClick={handleOnClick("bag", {key, bag_type})}>
               <h5 className="card-title">{bag_type}</h5>
               <p className="card-text"> 
                 <span>{count} Items unpacked</span>
