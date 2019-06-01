82814dd76cda9dcc9d8d96fcb595083b578084a2
Robert Abreu <robertabreu@pursuit.org>
2019-06-01 19:29:32 -0400

    moved mount logic to packingPage

--- src/services/packingPage.js
+++ src/services/packingPage.js
@@ -1,6 +1,41 @@
 import axios from "axios";
 import BASEURL from "./backendUrlConnect";
 
+export const mountPacking = async (bagTypes, bags) => {
+  const allBagPromise = [];
+  for (let bag of bags) {
+    allBagPromise.push(
+      axios({
+        method: "get",
+        url: `${BASEURL}/bag/${bag.bag_id}/all`
+      })
+    );
+  }
+  try {
+    const allBags = await Promise.all(allBagPromise);
+    const addToState = {};
+    let displayBag = "";
+    let totalItems = 0;
+    let totalPacked = 0;
+    for (let i = 0; i < allBags.length; i++) {
+      const { data: items } = allBags[i];
+      const { trip_id, bag_id, type_id } = bags[i];
+      const key = `${bagTypes[type_id].slice(0, 2)}${trip_id}${bag_id}`;
+      if (bagTypes[type_id] === "Personal") displayBag = key;
+      addToState[key] = items;
+      totalItems += items.length;
+      let count = items.reduce((a, e) => {
+        if (e.packed) a += 1;
+        return a;
+      }, 0);
+      totalPacked += count;
+    }
+    return { ...addToState, displayBag, totalItems, totalPacked };
+  } catch (err) {
+    return null;
+  }
+};
+
 export const addToDelete = (name, index, toDelete, displayBag, currentBag) => {
   const item_id = currentBag[index].item_id;
   let newToDelete = toDelete;
