47635768ff96c2e0e54a789ad5d5c496300060bb
Robert Abreu <robertabreu@pursuit.org>
2019-06-01 19:30:14 -0400

    imported mount logic from services, cleaned up componentMount

--- src/containers/PackingOverview/PackingPage/PackingPage.js
+++ src/containers/PackingOverview/PackingPage/PackingPage.js
@@ -10,6 +10,7 @@ import {
   addToDelete,
   executeDelete,
   markImportant,
+  mountPacking,
   unpack
 } from "../../../services/packingPage";
 import "./PackingPage.css";
@@ -39,38 +40,9 @@ export default (class PackPage extends Component {
   async componentDidMount() {
     const { bagTypes } = this.state;
     const { bags } = this.props;
-    const allBagPromise = [];
-    for (let bag of bags) {
-      allBagPromise.push(
-        axios({
-          method: "get",
-          url: `${BASEURL}/bag/${bag.bag_id}/all`
-        })
-      );
-    }
-    try {
-      const allBags = await Promise.all(allBagPromise);
-      const addToState = {};
-      let displayBag = "";
-      let totalItems = 0;
-      let totalPacked = 0;
-      for (let i = 0; i < allBags.length; i++) {
-        const { data: items } = allBags[i];
-        const { trip_id, bag_id, type_id } = bags[i];
-        const key = `${bagTypes[type_id].slice(0, 2)}${trip_id}${bag_id}`;
-        if (bagTypes[type_id] === "Personal") displayBag = key;
-        addToState[key] = items;
-        totalItems += items.length;
-        let count = items.reduce((a, e) => {
-          if (e.packed) a += 1;
-          return a;
-        }, 0);
-        totalPacked += count;
-      }
-      this.setState({ ...addToState, displayBag, totalItems, totalPacked });
-    } catch (err) {
-      console.log("Componenet Mount Error: ", err);
-    }
+    const mountState = await mountPacking(bagTypes, bags);
+    if (mountState) this.setState(mountState);
+    else console.log("Componenet Mount Error: ");
   }
 
   handleOnClick = (name, index) => e => {
