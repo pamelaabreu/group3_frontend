9407b5b16d655dafd7dca4b17d0c74768799991e
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-31 20:41:15 -0400

    Imported the BundleLogo as used it for the img src

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { pushRotate as Menu } from "react-burger-menu";
 import "./Navbar.css";
+import BundleLogo from "../../assets/images/logo/bundle_logo.svg";
 
 // Pages
 import ShowLoginOrSignup from "../../containers/ShowLoginOrSignup/ShowLoginOrSignup";
@@ -18,16 +19,15 @@ const Navbar = props => {
   };
 
   return (
-    <Menu>
-      {/* <Link to="/">
-        <div className="bundleNavbarLogo">Logo</div>
-      </Link> */}
-      <Link className="menu-item" to="/">
-        Home
+    <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
+      <Link to="/">
+        <img src={BundleLogo} width="150" height="150" alt="Bundle" />
       </Link>
+      <Link to="/">Home</Link>
       <Link to="packHacks">Pack Hacks</Link>
       <Link to="/tsaGuides">TSA Guides</Link>
       <Link to="/about">About</Link>
+
       {!FirebaseUserAuth.user ? (
         <ShowLoginOrSignup />
       ) : (
