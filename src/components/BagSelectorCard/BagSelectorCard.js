467ffd21e9c2f2b1c65d056fe2a36ece066f67a3
Robert Abreu <robertabreu@pursuit.org>
2019-05-31 22:17:02 -0400

    item count and key are now passed through more efficiently; cleaned up component

--- src/components/BagSelectorCard/BagSelectorCard.js
+++ src/components/BagSelectorCard/BagSelectorCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 export default (props => {
-    const {trip_id, bag_id, bag_type, item_count, handleOnClick} = props;
+    const { bag_type, handleOnClick, countAndKey: {count, key}} = props;
     return (
           <div className="col-3 mx-1 mt-2 card p-0 pack--bag-width">
-            <button className="card-body row" onClick={handleOnClick("bag", `${bag_type.slice(0,2)}${trip_id}${bag_id}`)}>
+            <button className="card-body row" onClick={handleOnClick("bag", key)}>
               <h5 className="card-title">{bag_type}</h5>
               <p className="card-text"> 
-                <span>{item_count} Items unpacked</span>
+                <span>{count} Items unpacked</span>
               </p>
             </button>
         </div>
