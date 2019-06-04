0546f40048602aee23ab2504594392a14fd1b74b
Srirupa <srirupadatta@pursuit.org>
2019-06-04 00:51:15 -0400

    destructured additional props; attached event listener to span tag

--- src/containers/PackingOverview/RemindersPage/Todo/Todo.js
+++ src/containers/PackingOverview/RemindersPage/Todo/Todo.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 const Todo = props => {
-  const { task_name } = props;
+  const { task_name, index, value, handleDeleteTodo } = props;
 
   return (
     <ul className="list-group">
       <li className="list-group-item d-flex justify-content-between align-items-center">
         {task_name}
-        <span className="badge badge-primary badge-pill">X</span>
+        <span
+          onClick={() => handleDeleteTodo(index, value)}
+          className="badge badge-primary badge-pill"
+        >
+          X
+        </span>
       </li>
     </ul>
   );
