032667cc8b50f00cb3123e4cc3a56dd64fc73f50
Robert Abreu <robertabreu@pursuit.org>
2019-06-05 13:24:03 -0400

    unpackeditem size increased

--- src/components/UnpackedItem/UnpackedItem.js
+++ src/components/UnpackedItem/UnpackedItem.js
@@ -24,86 +24,92 @@ export default props => {
     ? image
     : "https://www.jcrew.com/s7-img-facade/L4012_PA6511?fmt=jpeg";
   return (
-    <div className="col m-2 border border-white rounded">
-      <button
-        className={"item--unpacked p-0 rounded" + toDelete}
-        onClick={handleClick("item", index)}
-      >
-        <div className="container">
-          <div className={"row align-items-center justify-content-center"}>
-            <div>
-              {/* ITEM IMAGE */}
-              <img src={img} className="item--image--size rounded" alt={name} />
-            </div>
-            <div className="item--icon align-items-center">
-              {/* ICONS */}
+    <div className="col-6 col-md-3">
+      <div className="my-2 border border-white rounded">
+        <button
+          className={"item--unpacked p-0 rounded" + toDelete}
+          onClick={handleClick("item", index)}
+        >
+          <div className="container">
+            <div className={"row align-items-center justify-content-center"}>
               <div>
-                <i
-                  className="fas fa-shopping-cart item--icon--size"
-                  onClick={handleClick("shopping-cart", index)}
+                {/* ITEM IMAGE */}
+                <img
+                  src={img}
+                  className="item--image--size rounded"
+                  alt={name}
                 />
               </div>
-              <div>
-                {important ? (
+              <div className="item--icon align-items-center">
+                {/* ICONS */}
+                <div>
                   <i
-                    className="fas fa-star item--icon--size"
-                    onClick={handleClick("important", index)}
+                    className="fas fa-shopping-cart item--icon--size"
+                    onClick={handleClick("shopping-cart", index)}
                   />
-                ) : (
-                  <i
-                    className="far fa-star item--icon--size"
-                    onClick={handleClick("important", index)}
-                  />
-                )}
-              </div>
-              <div>
-                {flag ? (
-                  <i className="far fa-question-circle item--icon--size" />
-                ) : (
+                </div>
+                <div>
+                  {important ? (
+                    <i
+                      className="fas fa-star item--icon--size"
+                      onClick={handleClick("important", index)}
+                    />
+                  ) : (
+                    <i
+                      className="far fa-star item--icon--size"
+                      onClick={handleClick("important", index)}
+                    />
+                  )}
+                </div>
+                <div>
+                  {flag ? (
+                    <i className="far fa-question-circle item--icon--size" />
+                  ) : (
+                    /* needs work */
+                    <div className="item--icon-filler"> </div>
+                  )
                   /* needs work */
-                  <div className="item--icon-filler"> </div>
-                )
-                /* needs work */
-                }
+                  }
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="col-12">
-          {/* Item Name */}
-          <div
-            className={
-              "row align-items-center justify-content-center item--description-flow"
-            }
-          >
-            <span className="item--title--font item--title--color">
-              {name} <i className="fas fa-times item--count--size" />
-              {modifyQuant ? (
-                <input
-                  className="item--input--count"
-                  min="1"
-                  max="25"
-                  type="number"
-                  onChange={handleChange("quantity", index)}
-                  onKeyDown={onKeyPress("quantity", index)}
-                  value={quantity}
-                />
-              ) : (
-                <span onClick={handleClick("quantity", index)}>
-                  {" "}
-                  {quantity}
-                </span>
-              )}
-            </span>
+          <div className="col-12">
+            {/* Item Name */}
+            <div
+              className={
+                "row align-items-center justify-content-center item--description-flow"
+              }
+            >
+              <span className="item--title--font item--title--color">
+                {name} <i className="fas fa-times item--count--size" />
+                {modifyQuant ? (
+                  <input
+                    className="item--input--count"
+                    min="1"
+                    max="25"
+                    type="number"
+                    onChange={handleChange("quantity", index)}
+                    onKeyDown={onKeyPress("quantity", index)}
+                    value={quantity}
+                  />
+                ) : (
+                  <span onClick={handleClick("quantity", index)}>
+                    {" "}
+                    {quantity}
+                  </span>
+                )}
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="col-12" onClick={handleClick("select", index)}>
-          {/* Item Pack On / Off */}
-          <div className={toBePacked}>
-            <span className="item--pack--font">Pack</span>
+          <div className="col-12" onClick={handleClick("select", index)}>
+            {/* Item Pack On / Off */}
+            <div className={toBePacked}>
+              <span className="item--pack--font">Pack</span>
+            </div>
           </div>
-        </div>
-      </button>
+        </button>
+      </div>
     </div>
   );
 };
