5ac41fc334e8c6633163ac31db37e66848b1a7d7
Srirupa <srirupadatta@pursuit.org>
2019-06-05 21:44:37 -0400

    removed column styling

--- src/containers/PackingOverview/RemindersPage/ListCard/ListCard.js
+++ src/containers/PackingOverview/RemindersPage/ListCard/ListCard.js
@@ -4,7 +4,7 @@ const ListCard = props => {
   const { list_type, name, list_count } = props;
 
   return (
-    <div className="card col-3 mx-1 mt-2 p-0 pack--bag-width">
+    <div className="card mx-1 mt-2 p-0 pack--bag-width">
       <button className="card-body row">
         <h5 className="card-title">{list_type}</h5>
         <p className="card-text">
