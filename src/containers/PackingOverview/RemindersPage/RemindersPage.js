236dc24b9aa2d285e78dd553cf3c46b6fc246295
Srirupa <srirupadatta@pursuit.org>
2019-06-05 21:49:05 -0400

    added conditional rendering of todos based on complete status

--- src/containers/PackingOverview/RemindersPage/RemindersPage.js
+++ src/containers/PackingOverview/RemindersPage/RemindersPage.js
@@ -237,18 +237,22 @@ const RemindersPage = props => {
                 className="collapse multi-collapse"
                 id="multiCollapseExample3"
               >
-                <div className="card card-body">
+                <div className="">
                   {todoList.map((e, i, a) => {
                     let t = a[a.length - 1 - i];
-                    return (
-                      <Todo
-                        task_name={t.task_name}
-                        key={i}
-                        index={a.length - 1 - i}
-                        value={t.id}
-                        handleDeleteTodo={handleDeleteTodo}
-                      />
-                    );
+                    if (t.complete === false) {
+                      return (
+                        <Todo
+                          task_name={t.task_name}
+                          key={i}
+                          index={a.length - 1 - i}
+                          value={t.id}
+                          handleCompleteTodo={handleCompleteTodo}
+                          handleDeleteTodo={handleDeleteTodo}
+                          complete={t.complete}
+                        />
+                      );
+                    }
                   })}
                 </div>
               </div>
@@ -258,17 +262,25 @@ const RemindersPage = props => {
                 className="collapse multi-collapse"
                 id="multiCollapseExample2"
               >
-                <div className="card card-body">Completed</div>
+                <div className="">
+                  {todoList.map((e, i, a) => {
+                    let t = a[a.length - 1 - i];
+                    if (t.complete === true) {
+                      return (
+                        <Todo
+                          task_name={t.task_name}
+                          key={i}
+                          index={a.length - 1 - i}
+                          value={t.id}
+                          handleDeleteTodo={handleDeleteTodo}
+                          complete={t.complete}
+                        />
+                      );
+                    }
+                  })}
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="col-2">
-            <AddItemButton
-              itemInput={itemInput}
-              handleOnChange={handleItemInputChange}
-              handleCreateItem={handleCreateItem}
-            />
           </div>
         </div>
       </div>
