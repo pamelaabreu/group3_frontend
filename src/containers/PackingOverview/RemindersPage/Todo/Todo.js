e85e40b8da3e0b47f2374e1252ef4b8cd31e90aa
Srirupa <srirupadatta@pursuit.org>
2019-06-05 21:50:18 -0400

    rearranged content layout

--- src/containers/PackingOverview/RemindersPage/Todo/Todo.js
+++ src/containers/PackingOverview/RemindersPage/Todo/Todo.js
@@ -1,19 +1,35 @@
 import React from "react";
 
 const Todo = props => {
-  const { task_name, index, value, handleDeleteTodo } = props;
+  const {
+    task_name,
+    index,
+    value,
+    handleCompleteTodo,
+    handleDeleteTodo,
+    complete
+  } = props;
+
+  const completed = <i className="far fa-check-circle" />;
+  const incomplete = (
+    <i className="far fa-circle" onClick={handleCompleteTodo} />
+  );
 
   return (
     <ul className="list-group">
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        <i className="far fa-circle" />
-        {task_name}
-        <span
-          onClick={() => handleDeleteTodo(index, value)}
-          className="badge badge-primary badge-pill"
-        >
-          <i className="far fa-times-circle" />{" "}
-        </span>
+      <li className="px-0 list-group-item d-flex">
+        <div className="col-12 row justify-content-between no-gutters">
+          <div className="col-2 text-center">
+            <i
+              className="far fa-trash-alt"
+              onClick={() => handleDeleteTodo(index, value)}
+            />
+          </div>
+          <div className="col-8">{task_name}</div>
+          <div className="col-2 text-right">
+            {complete === false ? incomplete : completed}
+          </div>
+        </div>
       </li>
     </ul>
   );
