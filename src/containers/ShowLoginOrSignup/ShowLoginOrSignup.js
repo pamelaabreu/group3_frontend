d64ea3c84801607a55f5704f1865cacec4c3e154
Pamela Abreu <pamelaabreu@pursuit.com>
2019-06-05 21:46:43 -0400

    Added bg-transparent to login/signup button in ShowLoginOrSignup

--- src/containers/ShowLoginOrSignup/ShowLoginOrSignup.js
+++ src/containers/ShowLoginOrSignup/ShowLoginOrSignup.js
@@ -15,13 +15,13 @@ const ShowLoginOrSignup = props => {
     <div className="bg-white80 b-radius9 ds-lightGrey m-5 p-5">
       <div className="d-flex justify-content-center">
         <button
-          className={`c-bundleBlue p-2 border-0 ${loginActiveStyle}`}
+          className={`c-bundleBlue p-2 border-0 bg-transparent ${loginActiveStyle}`}
           onClick={() => setLoginOrSignup(true)}
         >
           Login
         </button>
         <button
-          className={`c-bundleBlue p-2 border-0 ${signupActiveStyle}`}
+          className={`c-bundleBlue p-2 border-0 bg-transparent ${signupActiveStyle}`}
           onClick={() => setLoginOrSignup(false)}
         >
           Signup
