095b57e8e71eb1a88cae1d5da2fbbd7c38cfcf88
Robert Abreu <robertabreu@pursuit.org>
2019-06-08 14:16:22 -0400

    UnpackedItemsV2 now uses hooks to adjust item size based on the width of the window

--- src/components/UnpackedItemV2/UnpackedItemV2.js
+++ src/components/UnpackedItemV2/UnpackedItemV2.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./UnpackedItemV2.css";
 
 export default props => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
   const {
     // flag,
     handleClick,
@@ -11,19 +19,31 @@ export default props => {
     index,
     toBeDeleted
   } = props;
+
   const toDelete = toBeDeleted ? " bg-danger text-white" : " ";
   const img = image
     ? image
     : "https://www.jcrew.com/s7-img-facade/L4012_PA6511?fmt=jpeg";
 
+  const itemWidth = () => {
+    if (width < 500) return "uItem--unpacked";
+    if (width > 500 && width < 990) return "uItem--unpacked-md";
+    if (width > 990 && width < 1200) return "uItem--unpacked-lg";
+    if (width > 1150 && width < 1300) return "uItem--unpacked-xlg";
+    if (width > 1300) return "uItem--unpacked-xxlg";
+  };
+
   return (
-    <div className="col-6 col-md-3 my-2 p-2">
+    <div className="col-6 col-sm-6 col-md-4 col-lg-4 my-2 p-2">
       <div
         className="row justify-content-center"
         onClick={handleClick("item", index)}
       >
         <div
-          className="uItem--unpacked my-2 border border-white uItem--content-main row no-gutters"
+          className={
+            itemWidth() +
+            "  my-2 border border-white uItem--content-main row no-gutters"
+          }
           style={{ backgroundImage: `url(${img})` }}
         >
           <div className="col-12 uItem--title text-center p-0 rounded">
