7b100c94db7f5cb825b2ebcfbf9b2c78cdda1b77
Srirupa <srirupadatta@pursuit.org>
2019-06-08 20:45:40 -0400

    rearranged layout of collapsible blocks

--- src/containers/PackingOverview/RemindersPage/TodoListView/TodoListView.js
+++ src/containers/PackingOverview/RemindersPage/TodoListView/TodoListView.js
@@ -12,81 +12,80 @@ const TodoListView = ({
 }) => {
   return (
     <>
-      <div>
-        <p>
-          <a
-            className="btn btn-primary"
-            data-toggle="collapse"
-            href="#multiCollapseExample3"
-            role="button"
-            aria-expanded="false"
-            aria-controls="multiCollapseExample3"
-          >
-            To Be Completed
-          </a>
-          <button
-            className="btn btn-primary ml-2"
-            type="button"
-            data-toggle="collapse"
-            data-target="#multiCollapseExample2"
-            aria-expanded="false"
-            aria-controls="multiCollapseExample2"
-          >
-            Completed
-          </button>
-        </p>
+      <div className="ml-2">
+        <a
+          className="col-4 btn btn-outline-primary text-left"
+          data-toggle="collapse"
+          href="#multiCollapseExample3"
+          role="button"
+          aria-expanded="false"
+          aria-controls="multiCollapseExample3"
+        >
+          To Be Completed
+        </a>
       </div>
 
-      <div className="row">
-        <div className="col">
-          <div
-            className="collapse show multi-collapse"
-            id="multiCollapseExample3"
-          >
-            <div className="">
-              {todoList.map((e, i, a) => {
-                let t = a[a.length - 1 - i];
-                if (t.complete === false) {
-                  return (
-                    <Todo
-                      task_name={t.task_name}
-                      key={i}
-                      index={a.length - 1 - i}
-                      value={t.id}
-                      handleCompleteTodo={handleCompleteTodo}
-                      handleDeleteTodo={handleDeleteTodo}
-                      complete={t.complete}
-                    />
-                  );
-                }
-                return null;
-              })}
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="collapse multi-collapse" id="multiCollapseExample2">
-            <div className="">
-              {todoList.map((e, i, a) => {
-                let t = a[a.length - 1 - i];
-                if (t.complete === true) {
-                  return (
-                    <Todo
-                      task_name={t.task_name}
-                      key={i}
-                      index={a.length - 1 - i}
-                      value={t.id}
-                      handleDeleteTodo={handleDeleteTodo}
-                      complete={t.complete}
-                    />
-                  );
-                }
-                return null;
-              })}
-            </div>
+      <div className="col">
+        <div
+          className="collapse show multi-collapse"
+          id="multiCollapseExample3"
+        >
+          <div className="">
+            {todoList.map((e, i, a) => {
+              let t = a[a.length - 1 - i];
+              if (t.complete === false) {
+                return (
+                  <Todo
+                    task_name={t.task_name}
+                    key={i}
+                    index={a.length - 1 - i}
+                    value={t.id}
+                    handleCompleteTodo={handleCompleteTodo}
+                    handleDeleteTodo={handleDeleteTodo}
+                    complete={t.complete}
+                  />
+                );
+              }
+              return null;
+            })}
           </div>
         </div>
       </div>
+
+      <button
+        className="col-4 btn btn-outline-primary ml-2 text-left"
+        type="button"
+        data-toggle="collapse"
+        data-target="#multiCollapseExample2"
+        aria-expanded="false"
+        aria-controls="multiCollapseExample2"
+      >
+        Completed
+      </button>
+
+      <div className="col">
+        <div className="collapse multi-collapse" id="multiCollapseExample2">
+          <div className="">
+            {todoList.map((e, i, a) => {
+              let t = a[a.length - 1 - i];
+              if (t.complete === true) {
+                return (
+                  <Todo
+                    task_name={t.task_name}
+                    key={i}
+                    index={a.length - 1 - i}
+                    value={t.id}
+                    handleDeleteTodo={handleDeleteTodo}
+                    complete={t.complete}
+                  />
+                );
+              }
+              return null;
+            })}
+          </div>
+        </div>
+      </div>
+
       <div>
         <AddTodo
           todoInput={todoInput}
