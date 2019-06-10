e30a1e1caf7f903e795ccb55b1b519f8b9996b46
Robert Abreu <robertabreu@pursuit.org>
2019-06-10 10:57:35 -0400

    wrapped tabs row and assigned columns

--- src/components/PackingTabs/PackingTabs.js
+++ src/components/PackingTabs/PackingTabs.js
@@ -2,19 +2,29 @@ import React from "react";
 import "./PackingTabs.css";
 
 export default ({ page, handleOnClick, moveToTrip, windowHeight }) => {
-  const packingStyle = page === "packing" ? "" : "text-muted";
-  const remindersStyle = page === "packing" ? "text-muted" : "";
+  const packingStyle = ""; //page === "packing" ? "" : "text-muted";
+  const remindersStyle = ""; //page === "packing" ? "text-muted" : "";
   const height = Math.floor(windowHeight / 15);
   return (
     <div className="container" style={{ height: `${height}px` }}>
       <div className="row tabs--main">
-        <div className="col-10">
-          <button className="btn" onClick={handleOnClick("packing")}>
-            <span className={"" + packingStyle}>Packing</span>
-          </button>
-          <button className="btn " onClick={handleOnClick("reminders")}>
-            <span className={"" + remindersStyle}>Reminders</span>
-          </button>
+        <div className="col-8 offset-2 tabs--header">
+          <div className="row">
+            <button
+              type="button"
+              className="col tabs--button"
+              onClick={handleOnClick("packing")}
+            >
+              <span className={" " + packingStyle}>Packing</span>
+            </button>
+            <button
+              type="button"
+              className="col tabs--button"
+              onClick={handleOnClick("reminders")}
+            >
+              <span className={" " + remindersStyle}>Reminders</span>
+            </button>
+          </div>
         </div>
         <div className="col-2">
           <button className="row" onClick={moveToTrip}>
