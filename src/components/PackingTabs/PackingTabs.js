46842be5f2cb5c5bc22f9c2f797106c11c394e38
Robert Abreu <robertabreu@pursuit.org>
2019-06-10 12:51:47 -0400

    added aria pressed and labels to buttons

--- src/components/PackingTabs/PackingTabs.js
+++ src/components/PackingTabs/PackingTabs.js
@@ -2,24 +2,29 @@ import React from "react";
 import "./PackingTabs.css";
 
 export default ({ page, handleOnClick, moveToTrip, windowHeight }) => {
-  const packingStyle = ""; //page === "packing" ? "" : "text-muted";
-  const remindersStyle = ""; //page === "packing" ? "text-muted" : "";
+  const packingStyle = page === "packing" ? "tabs-active" : "tabs-inactive";
+  const remindersStyle = page === "packing" ? "tabs-inactive" : "tabs-active";
+  const ariaPressed = page === "packing";
   const height = Math.floor(windowHeight / 15);
   return (
     <div className="container" style={{ height: `${height}px` }}>
-      <div className="row tabs--main">
-        <div className="col-8 offset-2 tabs--header">
+      <div className="row tabs--main no-gutters">
+        <div className="pl-2 col-8 offset-2 tabs--header">
           <div className="row">
             <button
               type="button"
-              className="col tabs--button"
+              aria-pressed={ariaPressed}
+              aria-label="Packing Page"
+              className="col-4 tabs--button pr-5"
               onClick={handleOnClick("packing")}
             >
               <span className={" " + packingStyle}>Packing</span>
             </button>
             <button
               type="button"
-              className="col tabs--button"
+              aria-label="Reminders Page"
+              aria-pressed={!ariaPressed}
+              className="col-4 tabs--button pl-4"
               onClick={handleOnClick("reminders")}
             >
               <span className={" " + remindersStyle}>Reminders</span>
@@ -27,9 +32,14 @@ export default ({ page, handleOnClick, moveToTrip, windowHeight }) => {
           </div>
         </div>
         <div className="col-2">
-          <button className="row" onClick={moveToTrip}>
-            <span className="col-12 text-center">Trip</span>
-            <i className="col-12 fas fa-long-arrow-alt-left text-center pack--arrow-transform" />
+          <button
+            type="button"
+            aria-label="Trip Details Page"
+            className="row tabs--button"
+            onClick={moveToTrip}
+          >
+            <span className="col-12 text-left tabs--trip">Trip</span>
+            <span className="col-12 text-left tabs--details">Details</span>
           </button>
         </div>
       </div>
