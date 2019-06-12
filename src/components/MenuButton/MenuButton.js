ae8c75974b5e7848c7316b1c473ac68cb3af286f
Robert Abreu <robertabreu@pursuit.org>
2019-06-12 13:12:49 -0400

    implemented ability to pass down attributes to MenuButtons

--- src/components/MenuButton/MenuButton.js
+++ src/components/MenuButton/MenuButton.js
@@ -6,12 +6,15 @@ export default ({
   faClass,
   classes,
   iconClasses,
-  clickCommand
+  clickCommand,
+  attributes
 }) => {
+  const attr = attributes ? attributes : {};
   return (
     <button
       className={"mbutton--container rounded-circle shadow " + classes}
       onClick={handleOnClick(clickCommand)}
+      {...attr}
     >
       <i className={`px-3 py-3 mbutton--icon ${faClass} ` + iconClasses} />
     </button>
