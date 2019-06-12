67a798b2dd1a87588a5c0943560b11dcf297e253
Robert Abreu <robertabreu@pursuit.org>
2019-06-12 12:53:55 -0400

    added another prop clickComman for handleOnClick, should be a string; passed in handleOnClick which is invoked with clickCommand as argument, faClass now has to be the full font awesome class name

--- src/components/MenuButton/MenuButton.js
+++ src/components/MenuButton/MenuButton.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./MenuButton.css";
 
-export default ({ handleOnClick, faClass, classes, iconClasses }) => {
+export default ({
+  handleOnClick,
+  faClass,
+  classes,
+  iconClasses,
+  clickCommand
+}) => {
   return (
     <button
       className={"mbutton--container rounded-circle shadow " + classes}
-      onClick={handleOnClick}
+      onClick={handleOnClick(clickCommand)}
     >
-      <i className={`px-3 py-3 mbutton--icon fas ${faClass} ` + iconClasses} />
+      <i className={`px-3 py-3 mbutton--icon ${faClass} ` + iconClasses} />
     </button>
   );
 };
