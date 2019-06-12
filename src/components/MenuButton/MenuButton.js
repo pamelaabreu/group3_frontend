4eaa9b64ef27849a68a47e51d67f11445b86c1c3
Robert Abreu <robertabreu@pursuit.org>
2019-06-12 12:18:47 -0400

    added minimum menu button styling

--- src/components/MenuButton/MenuButton.js
+++ src/components/MenuButton/MenuButton.js
@@ -3,8 +3,11 @@ import "./MenuButton.css";
 
 export default ({ handleOnClick, faClass, classes, iconClasses }) => {
   return (
-    <button className={"mbutton--container " + classes} onClick={handleOnClick}>
-      <i className={`fas ${faClass} ` + iconClasses} />
+    <button
+      className={"mbutton--container rounded-circle shadow " + classes}
+      onClick={handleOnClick}
+    >
+      <i className={`px-3 py-3 mbutton--icon fas ${faClass} ` + iconClasses} />
     </button>
   );
 };
