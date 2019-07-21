5ebcf99654bc7ab01a12c24cf02f37694c7450c3
Pamela Abreu <pamelaabreu@pursuit.com>
2019-07-21 12:25:26 -0400

    Created the PackHacks intial component

---
+++ src/components/PackHacks/PackHacks.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PackHacks = () => {
+  return (
+    <div className="bg-bundleBlueBabyBlue min-vh-100 min-vw-100">
+      <header className="container p-5">
+        <h1 className="c-white mali900 display-3 text-center">Pack Hacks</h1>
+      </header>
+
+      <div className="container overflow-auto m-5 p-5">//... the cards</div>
+    </div>
+  );
+};
+
+export default PackHacks;
