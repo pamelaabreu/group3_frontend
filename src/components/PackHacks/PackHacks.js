1063516325c856074b6b04ed5f0680682a7a6ecb
Pamela Abreu <pamelaabreu@pursuit.com>
2019-07-21 13:00:28 -0400

    Created the PackHacksCardRender variable to render through an array with PackHacks card info

--- src/components/PackHacks/PackHacks.js
+++ src/components/PackHacks/PackHacks.js
@@ -3,6 +3,24 @@ import PackHacksCard from "../PackHacksCard/PackHacksCard";
 import PackHacksJSON from "../../assets/json/packHacks";
 
 const PackHacks = () => {
+  const PackHacksKeys = Object.keys(PackHacksJSON);
+
+  const PackHacksCardRender = PackHacksKeys.map((keyname, index) => {
+    const title = keyname;
+    const tipNumber = index + 1;
+    const imgURL = PackHacksJSON[keyname].imgURL;
+    const description = PackHacksJSON[keyname].description;
+    return (
+      <PackHacksCard
+        key={index}
+        title={title}
+        tipNumber={tipNumber}
+        imgURL={imgURL}
+        description={description}
+      />
+    );
+  });
+
   return (
     <div className="bg-bundleBlueBabyBlue min-vh-100 min-vw-100">
       <header className="container p-5">
@@ -10,7 +28,7 @@ const PackHacks = () => {
       </header>
 
       <div className="container overflow-auto m-5 p-5">
-        <PackHacksCard />
+        {PackHacksCardRender}
       </div>
     </div>
   );
