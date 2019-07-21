4b367a2e95748135a927f183f75a7c2c87f5580d
Pamela Abreu <pamelaabreu@pursuit.com>
2019-07-21 13:06:57 -0400

    Initial PackHacksCard component

---
+++ src/components/PackHacksCard/PackHacksCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PackHacksCard = ({ title, tipNumber, imgURL, description }) => {
+  return (
+    <div className="b-radius9 card border-0 mx-1 my-4 col-12 col-md-4 col-lg-5 p-0 ds-lightGrey">
+      // card body
+    </div>
+  );
+};
+
+export default PackHacksCard;
